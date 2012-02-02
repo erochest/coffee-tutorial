8fbfe28648ad0321287c3f7d4b84c1e8ae1faf70
Eric Rochester <err8n@virginia.edu>
2012-02-02 09:58:48 -0500

    Added page for working out Conway's life and a first pass at its implementation.

---
+++ js/life.coffee
@@ -0,0 +1,207 @@
+
+window.requestAnimFrame =
+  window.requestAnimationFrame       ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame    ||
+  window.oRequestAnimationFrame      ||
+  window.msRequestAnimationFrame     || 
+  (callback) -> window.setTimeout callback, 1000/60
+
+# We're using an explicit double buffer here. One is for the current state of
+# the world, the other is for the next state. They're kept in `ImageData`
+# objects generated directly from the canvas, and they're switched using the
+# 'draw' method.
+#
+# Currently, this assumes that the abstract grid maps directly to the pixel
+# grid.
+#
+# This also manages the canvas and its context. The only long-term reference to
+# the context is held by this class.
+
+class BufferedCanvas
+
+  # This initializes the `BufferedCanvas` with the `<canvas>` DOM object.
+  constructor: (canvas) ->
+    @canvas = $ canvas
+    this.reset()
+
+  # This resets the current and buffer after a change from outside this
+  # `BufferedCanvas`. Any changes in `buffer` will be thrown away. This also
+  # clears the canvas.
+  reset: ->
+    canvas = @canvas[0]
+
+    # Get the current height of the canvas.
+    @width   = @canvas.width()
+    @height  = @canvas.height()
+
+    # Set the DOM's height and width and get a new context.
+    canvas.width  = @width
+    canvas.height = @height
+    @context      = canvas.getContext '2d'
+
+    # Create the buffers from the context. Also supports method chaining.
+    this.resetBuffers()
+
+  # Just resets the buffers.
+  resetBuffers: ->
+    @current = @context.getImageData 0, 0, @width, @height
+    @buffer  = @context.createImageData @current
+    this
+
+  # This gets the index in the `ImageData` for position x and y.
+  index: (x, y) ->
+    4 * (y * @width + x)
+
+  # This gets the value of the current state for the x, y pixel and the color
+  # offset.
+  get: (x, y, colorOffset) ->
+    i = this.index x, y
+    @current.data[i + colorOffset]
+
+  # This is like `get`, only it queries the buffer.
+  getBuffer: (x, y, colorOffset) ->
+    i = this.index x, y
+    @buffer.data[i + colorOffset]
+
+  # This sets the value for the next state.
+  set: (x, y, red=0, green=0, blue=0, alpha=255) ->
+    i = this.index x, y
+    @buffer.data[i + 0] = red
+    @buffer.data[i + 1] = green
+    @buffer.data[i + 2] = blue
+    @buffer.data[i + 3] = alpha
+
+    # Method chaining
+    this
+
+  # This unsets the value for the next state. That is, it sets it to
+  # transparent black.
+  unset: (x, y) ->
+    this.set x, y, 0, 0, 0, 0
+    this
+
+  # This swaps out the buffer and makes the @buffer the current.
+  draw: ->
+    @context.putImageData @buffer, 0, 0
+    this.resetBuffers()
+
+  # This clears the canvas.
+  clear: ->
+    canvas = @canvas[0]
+    canvas.width = canvas.width
+    this
+
+# This manages the world.
+class Life
+  cellSize: 1
+  background: 'black'
+
+  # This takes the jQuery DOM objects for the world and the status `<div>`.
+  constructor: (@env, @status) ->
+    @buffer = new BufferedCanvas @env
+    @gen = 0
+    this.updateStatus "Conway's Life"
+
+  # This sets `n` random cells and draws the buffer.
+  #
+  # `n` can be an integer, which is the number of pixels to fill, or a
+  # float-point percent in the range [0-1].
+  randomFill: (n) ->
+    random = (x) -> Math.floor(Math.random() * x)
+
+    @buffer.reset()
+    width  = @buffer.width
+    height = @buffer.height
+
+    # If n is a percentage, then get the actual number of cells to fill in.
+    count = if Math.floor(n) == 0 then Math.floor(n * width * height) else n
+
+    while count > 0
+      i = random width
+      j = random height
+      if @buffer.getBuffer(i, j, 0) == 0
+        @buffer.set i, j, 255
+        count -= 1
+
+    this.draw()
+
+  # This runs one generation of the loop and requests the animation frame for
+  # the next generation.
+  run: ->
+    this.update()
+    this.draw()
+
+    @gen += 1
+    this.updateStatus "Generation: #{ @gen }"
+
+    # requestAnimFrame => this.run()
+
+  outline: ->
+    @buffer.reset()
+
+    count = 0
+    for x in [0..@buffer.width-1]
+      @buffer
+        .set(x, 0, 255)
+        .set(x, @buffer.height - 1, 255)
+      count += 2
+    for y in [0..@buffer.height-1]
+      @buffer
+        .set(0, y, 255)
+        .set(@buffer.width - 1, y, 255)
+      count += 2
+
+    log @buffer.width, @buffer.height, count
+    this.draw()
+
+  outlineRect: ->
+    @buffer.reset()
+
+    @buffer.context.strokeStyle = 'maroon'
+
+    bounds = [0, 0, @buffer.width - 1, @buffer.height - 1]
+    log bounds...
+    @buffer.context.strokeRect bounds...
+
+  # This updates the state for the next generation buffer.
+  update: ->
+    for i in [0..@buffer.width]
+      for j in [0..@buffer.height]
+        @buffer.set(i, j, 255) if this.next(i, j)
+
+  # This looks at the buffer and determines whether the given cell should be
+  # turned on or off for the next generation. It returns a bool, with `true`
+  # equal to on.
+  next: (i, j) ->
+    count = 0
+
+    for m in [i-1..i+1]
+      for n in [j-1..j+1]
+        if 0 <= m < @buffer.width and 0 <= n < @buffer.height
+          count += 1
+
+    switch count
+      when 2 then @buffer.get(i, j, 0) == 255
+      when 3 then true
+      else false
+
+  # This clears the canvas and draws the buffer.
+  draw: ->
+    @buffer.draw()
+
+  # This clears the canvas.
+  clear: ->
+    @buffer.clear()
+
+  # This updates the status message.
+  updateStatus: (msg) ->
+    @status.html msg
+
+
+life = new Life $('#environment'), $('#status')
+life.randomFill 0.25
+life.run()
+
+window.life = life
+
---
+++ js/life.js
@@ -0,0 +1,213 @@
+(function() {
+  var BufferedCanvas, Life, life;
+
+  window.requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+    return window.setTimeout(callback, 1000 / 60);
+  };
+
+  BufferedCanvas = (function() {
+
+    function BufferedCanvas(canvas) {
+      this.canvas = $(canvas);
+      this.reset();
+    }
+
+    BufferedCanvas.prototype.reset = function() {
+      var canvas;
+      canvas = this.canvas[0];
+      this.width = this.canvas.width();
+      this.height = this.canvas.height();
+      canvas.width = this.width;
+      canvas.height = this.height;
+      this.context = canvas.getContext('2d');
+      return this.resetBuffers();
+    };
+
+    BufferedCanvas.prototype.resetBuffers = function() {
+      this.current = this.context.getImageData(0, 0, this.width, this.height);
+      this.buffer = this.context.createImageData(this.current);
+      return this;
+    };
+
+    BufferedCanvas.prototype.index = function(x, y) {
+      return 4 * (y * this.width + x);
+    };
+
+    BufferedCanvas.prototype.get = function(x, y, colorOffset) {
+      var i;
+      i = this.index(x, y);
+      return this.current.data[i + colorOffset];
+    };
+
+    BufferedCanvas.prototype.getBuffer = function(x, y, colorOffset) {
+      var i;
+      i = this.index(x, y);
+      return this.buffer.data[i + colorOffset];
+    };
+
+    BufferedCanvas.prototype.set = function(x, y, red, green, blue, alpha) {
+      var i;
+      if (red == null) red = 0;
+      if (green == null) green = 0;
+      if (blue == null) blue = 0;
+      if (alpha == null) alpha = 255;
+      i = this.index(x, y);
+      this.buffer.data[i + 0] = red;
+      this.buffer.data[i + 1] = green;
+      this.buffer.data[i + 2] = blue;
+      this.buffer.data[i + 3] = alpha;
+      return this;
+    };
+
+    BufferedCanvas.prototype.unset = function(x, y) {
+      this.set(x, y, 0, 0, 0, 0);
+      return this;
+    };
+
+    BufferedCanvas.prototype.draw = function() {
+      this.context.putImageData(this.buffer, 0, 0);
+      return this.resetBuffers();
+    };
+
+    BufferedCanvas.prototype.clear = function() {
+      var canvas;
+      canvas = this.canvas[0];
+      canvas.width = canvas.width;
+      return this;
+    };
+
+    return BufferedCanvas;
+
+  })();
+
+  Life = (function() {
+
+    Life.prototype.cellSize = 1;
+
+    Life.prototype.background = 'black';
+
+    function Life(env, status) {
+      this.env = env;
+      this.status = status;
+      this.buffer = new BufferedCanvas(this.env);
+      this.gen = 0;
+      this.updateStatus("Conway's Life");
+    }
+
+    Life.prototype.randomFill = function(n) {
+      var count, height, i, j, random, width;
+      random = function(x) {
+        return Math.floor(Math.random() * x);
+      };
+      this.buffer.reset();
+      width = this.buffer.width;
+      height = this.buffer.height;
+      count = Math.floor(n) === 0 ? Math.floor(n * width * height) : n;
+      while (count > 0) {
+        i = random(width);
+        j = random(height);
+        if (this.buffer.getBuffer(i, j, 0) === 0) {
+          this.buffer.set(i, j, 255);
+          count -= 1;
+        }
+      }
+      return this.draw();
+    };
+
+    Life.prototype.run = function() {
+      this.update();
+      this.draw();
+      this.gen += 1;
+      return this.updateStatus("Generation: " + this.gen);
+    };
+
+    Life.prototype.outline = function() {
+      var count, x, y, _ref, _ref2;
+      this.buffer.reset();
+      count = 0;
+      for (x = 0, _ref = this.buffer.width - 1; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
+        this.buffer.set(x, 0, 255).set(x, this.buffer.height - 1, 255);
+        count += 2;
+      }
+      for (y = 0, _ref2 = this.buffer.height - 1; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
+        this.buffer.set(0, y, 255).set(this.buffer.width - 1, y, 255);
+        count += 2;
+      }
+      log(this.buffer.width, this.buffer.height, count);
+      return this.draw();
+    };
+
+    Life.prototype.outlineRect = function() {
+      var bounds, _ref;
+      this.buffer.reset();
+      this.buffer.context.strokeStyle = 'maroon';
+      bounds = [0, 0, this.buffer.width - 1, this.buffer.height - 1];
+      log.apply(null, bounds);
+      return (_ref = this.buffer.context).strokeRect.apply(_ref, bounds);
+    };
+
+    Life.prototype.update = function() {
+      var i, j, _ref, _results;
+      _results = [];
+      for (i = 0, _ref = this.buffer.width; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        _results.push((function() {
+          var _ref2, _results2;
+          _results2 = [];
+          for (j = 0, _ref2 = this.buffer.height; 0 <= _ref2 ? j <= _ref2 : j >= _ref2; 0 <= _ref2 ? j++ : j--) {
+            if (this.next(i, j)) {
+              _results2.push(this.buffer.set(i, j, 255));
+            } else {
+              _results2.push(void 0);
+            }
+          }
+          return _results2;
+        }).call(this));
+      }
+      return _results;
+    };
+
+    Life.prototype.next = function(i, j) {
+      var count, m, n, _ref, _ref2, _ref3, _ref4;
+      count = 0;
+      for (m = _ref = i - 1, _ref2 = i + 1; _ref <= _ref2 ? m <= _ref2 : m >= _ref2; _ref <= _ref2 ? m++ : m--) {
+        for (n = _ref3 = j - 1, _ref4 = j + 1; _ref3 <= _ref4 ? n <= _ref4 : n >= _ref4; _ref3 <= _ref4 ? n++ : n--) {
+          if ((0 <= m && m < this.buffer.width) && (0 <= n && n < this.buffer.height)) {
+            count += 1;
+          }
+        }
+      }
+      switch (count) {
+        case 2:
+          return this.buffer.get(i, j, 0) === 255;
+        case 3:
+          return true;
+        default:
+          return false;
+      }
+    };
+
+    Life.prototype.draw = function() {
+      return this.buffer.draw();
+    };
+
+    Life.prototype.clear = function() {
+      return this.buffer.clear();
+    };
+
+    Life.prototype.updateStatus = function(msg) {
+      return this.status.html(msg);
+    };
+
+    return Life;
+
+  })();
+
+  life = new Life($('#environment'), $('#status'));
+
+  life.randomFill(0.25);
+
+  life.run();
+
+  window.life = life;
+
+}).call(this);
